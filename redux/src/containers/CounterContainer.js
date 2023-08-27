c805c6701a458854e44d970bbf1c6d09df11e0f5
seungyeon1 <953dltmddus@gmail.com>
2023-08-27 12:18:33 +0900

    redux hook

--- redux/src/containers/CounterContainer.js
+++ redux/src/containers/CounterContainer.js
@@ -1,12 +1,14 @@
-import { connect } from "react-redux";
+// import { connect } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import Counter from "../components/Counter"; //UI
 import { decrease, increase } from "../modules/counter"; //모듈 연결의 변수 연결
+import React, { useCallback } from "react";
 
-const CounterContainer = ({ number, increase, decrease }) => {
-  return (
-    <Counter number={number} onIncrease={increase} onDecrease={decrease} />
-  );
-};
+// const CounterContainer = ({ number, increase, decrease }) => {
+//   return (
+//     <Counter number={number} onIncrease={increase} onDecrease={decrease} />
+//   );
+// };
 
 // const mapStateToProps = (state) => ({
 //   number: state.counter.number,
@@ -41,11 +43,36 @@ const CounterContainer = ({ number, increase, decrease }) => {
 // )(CounterContainer);
 
 // 줄 28 ~ 41 : 간소화 2
-export default connect(
-  // 상태
-  (state) => ({
-    number: state.counter.number,
-  }),
-  // 액션
-  { increase, decrease }
-)(CounterContainer);
+// export default connect(
+//   // 상태
+//   (state) => ({
+//     number: state.counter.number,
+//   }),
+//   // 액션
+//   { increase, decrease }
+// )(CounterContainer);
+
+// connect 함수 대신 connect hook 사용 방법
+const CounterContainer = () => {
+  const number = useSelector((state) => state.counter.number);
+  const dispatch = useDispatch(); //connect 함수 대신 hook 업데이트
+  //객체 만든 이유 : 공간을 만들었다.
+  const onIncrease = useCallback(() => dispatch(increase(), [dispatch])); //dispatch만 업데이트
+  const onDecrease = useCallback(() => dispatch(decrease(), [dispatch]));
+
+  // 리랜더링이 되므로 아래 useCallback 사용해보자 최적화 해주기
+  // return (
+  //   <Counter
+  //     number={number}
+  //     onIncrease={() => dispatch(increase())} //dispatch, increase 둘다 계속 랜더링이 된다.
+  //     onDecrease={() => dispatch(decrease())}
+  //   />
+  // );
+
+  //useCallback 사용후
+  return (
+    <Counter number={number} onIncrease={onIncrease} onDecrease={onDecrease} />
+  );
+};
+
+export default CounterContainer;
--- redux/src/containers/TodosContainer.js
+++ redux/src/containers/TodosContainer.js
@@ -1,36 +1,79 @@
-import { connect } from "react-redux";
+import { connect, useDispatch, useSelector } from "react-redux";
 import Todos from "../components/Todo"; //UI
 import { changeInput, insert, toggle, remove } from "../modules/todos"; //모듈의 변수 연결
+import React, { useCallback } from "react";
+import { useActions } from "../lib/useActions";
 
-const TodosContainer = ({
-  input,
-  todos,
-  changeInput,
-  insert,
-  toggle,
-  remove,
-}) => {
+// const TodosContainer = ({
+//   input,
+//   todos,
+//   changeInput,
+//   insert,
+//   toggle,
+//   remove,
+// }) => {
+//   return (
+//     <Todos
+//       input={input}
+//       todos={todos}
+//       onChangeInput={changeInput}
+//       onInsert={insert}
+//       onToggle={toggle}
+//       onRemove={remove}
+//     />
+//   );
+// };
+
+// export default connect(
+//   ({ todos }) => ({
+//     input: todos.input,
+//     todos: todos.todos,
+//   }),
+//   {
+//     changeInput,
+//     insert,
+//     toggle,
+//     remove,
+//   }
+// )(TodosContainer);
+
+// connect 함수 대신 connect hook 사용 방법
+// 리랜더링이 되므로 아래 useCallback 사용해보자 최적화 해주기
+const TodosContainer = () => {
+  //비구조화할당문
+  //변수를 상태값으로 연결 한다.
+  const { input, todos } = useSelector(({ todos }) => ({
+    input: todos.input,
+    todos: todos.todos,
+  }));
+
+  //hook 사용 방법
+  // const dispatch = useDispatch();
+  // const onChangeInput = useCallback(
+  //   (input) => dispatch(changeInput(input)),
+  //   [dispatch]
+  // );
+  // const onInsert = useCallback((text) => dispatch(insert(text)), [dispatch]);
+  // const onToggle = useCallback((id) => dispatch(toggle(id)), [dispatch]);
+  // const onRemove = useCallback((id) => dispatch(remove(id)), [dispatch]);
+
+  //useAction.js lib 사용버전
+  const [onChangeInput, onInsert, onToggle, onRemove] = useActions(
+    [changeInput, insert, toggle, remove],
+    []
+  );
+
   return (
     <Todos
       input={input}
       todos={todos}
-      onChangeInput={changeInput}
-      onInsert={insert}
-      onToggle={toggle}
-      onRemove={remove}
+      onChangeInput={onChangeInput}
+      onInsert={onInsert}
+      onToggle={onToggle}
+      onRemove={onRemove}
     />
   );
 };
 
-export default connect(
-  ({ todos }) => ({
-    input: todos.input,
-    todos: todos.todos,
-  }),
-  {
-    changeInput,
-    insert,
-    toggle,
-    remove,
-  }
-)(TodosContainer);
+// export default TodosContainer;
+export default React.momo(TodosContainer); //최적화..
