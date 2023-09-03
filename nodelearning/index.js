d68fd1a8bb61cc3265d77b7ec4380d7868793ffa
seungyeon1 <953dltmddus@gmail.com>
2023-09-03 11:44:37 +0900

    node js

--- nodelearning/index.js
+++ nodelearning/index.js
@@ -7,5 +7,13 @@
 // console.log(circle.PI, circle.getArea(2), circle.getCircumference(2));
 
 //export default를 사용하면
-import circle from "./circle.js";
-console.log(circle.PI, circle.getArea(2), circle.getCircumference(2));
+// import circle from "./circle.js";
+// console.log(circle.PI, circle.getArea(2), circle.getCircumference(2));
+
+// -------------------------- lodash
+import lodash from "lodash";
+
+const arr = [1, 1, 3, 4, 5, 6, 1, 2, 3, 5];
+const uniqueArr = lodash.uniqBy(arr); //중복된 배열 제거
+
+console.log(uniqueArr);
