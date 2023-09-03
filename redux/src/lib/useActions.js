66767a12d3c2285c42d0f767b292bf150edb0111
seungyeon1 <953dltmddus@gmail.com>
2023-09-03 11:58:27 +0900

    [] 추가 설명

--- redux/src/lib/useActions.js
+++ redux/src/lib/useActions.js
@@ -11,6 +11,12 @@ import { useMemo } from "react";
 //첫번째 파라미터는 액션 생성함수를 이루어진 배열
 //두번째 파라미터는 deps배열, 이배열안에 들어있는 원소가 바뀌면 액션을
 //디스패치하는 함수를 새로 만들게 된다.
+
+// deps : 의존성 배열
+// 리액트가 언제 컴포넌트를 다시 렌더링해야 하는지 결정하는데 사용, 배열 내의 값들이
+// 이전 렌더링과 비교되어 변경이 있으면 해당 훅 내의 내용이 실행되거나 값이 업데이트된다.
+// 변경이 없으면 리렌더링이 발생하지 않는다.
+
 export function useActions(actions, deps) {
   const dispatch = useDispatch();
   //useMemo 최적화
